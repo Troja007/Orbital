eed01697fbdc45e574286f0892d3a25a7b3a2192
Troja007 <thorsten.schranz@gmail.com>
2025-03-04 18:03:04 +0100

    changes

--- custom_queries/Firewall/Firewall_Log_Windows_Statistics.sql
+++ custom_queries/Firewall/Firewall_Log_Windows_Statistics.sql
@@ -2,10 +2,10 @@ SELECT
   REPLACE(REPLACE(REPLACE(SUBSTR(datetime, 1, 19), 'T', ' '), '.', ''), 'Z', '') AS "timestamp_evt",
   datetime(CAST(JSON_EXTRACT(data, '$.EventData.Timestamp') AS INTEGER) / 1000, 'unixepoch', 'localtime') AS "timestamp_fw",
   JSON_EXTRACT(json(data), '$.EventData.Protocol') AS "protocol",
-  JSON_EXTRACT(json(data), '$.EventData.LocalIP') AS "source-ip",
-  JSON_EXTRACT(json(data), '$.EventData.LocalPort') AS "source-port",
-  JSON_EXTRACT(json(data), '$.EventData.RemoteIP') AS "remote-ip",
-  JSON_EXTRACT(json(data), '$.EventData.RemotePort') AS "remote-port",
+  JSON_EXTRACT(json(data), '$.EventData.LocalIP') AS "sourceIp",
+  JSON_EXTRACT(json(data), '$.EventData.LocalPort') AS "sourcePort",
+  JSON_EXTRACT(json(data), '$.EventData.RemoteIP') AS "remoteIp",
+  JSON_EXTRACT(json(data), '$.EventData.RemotePort') AS "remotePort",
   CASE
     WHEN eventid = 1800 THEN 'Allow'
     WHEN eventid = 1801 THEN 'Block'
