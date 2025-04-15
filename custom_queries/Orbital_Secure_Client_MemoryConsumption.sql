b2ce8a2c4e47661d783f8976e35c361db2df9dff
Troja007 <thorsten.schranz@gmail.com>
2025-04-15 16:33:25 +0200

    changes

--- custom_queries/Orbital_Secure_Client_MemoryConsumption.sql
+++ custom_queries/Orbital_Secure_Client_MemoryConsumption.sql
@@ -1,17 +1,11 @@
 SELECT pid, name, 
-ROUND((resident_size / 1024 / 1024) ,0) AS "Memory WorkingSet MB",
-ROUND((total_size / 1024 / 1024) ,0) AS "Memory Commit MB",
-threads AS "Thread Count",
-ROUND((disk_bytes_read / 1024 / 1024 / 1024) ,0) AS "Disk Read GB",
-ROUND((disk_bytes_written / 1024 / 1024 / 1024) ,0) AS "Disk Write GB",
-ROUND((elapsed_time / 3600) ,0) AS "Uptime Hours",
+ROUND((resident_size / 1024 / 1024) ,0) AS "priv mem (MB)",
+ROUND((elapsed_time / 3600) ,0) AS "uptime hours",
 path
-
---ROUND((total_size * '10e-7') ,0) AS Wert_2
 FROM processes
 
 where path like "%cisco%"
-OR name in ("acnvmagent.exe", "cscm.exe")
+OR name in ("acnvmagent.exe", "cscm.exe", "csc_cmid.exe", "csc_cms", "csc_pm.exe", "orbital.exe", "osqueryd.exe", "sfc.exe", "vpnagent.exe")
 
 
 
