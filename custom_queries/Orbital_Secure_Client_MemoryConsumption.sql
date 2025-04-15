0520a1e9e1900f2d16b02def099ccd7fd6443432
Troja007 <thorsten.schranz@gmail.com>
2025-04-15 16:32:27 +0200

    new query

---
+++ custom_queries/Orbital_Secure_Client_MemoryConsumption.sql
@@ -0,0 +1,19 @@
+SELECT pid, name, 
+ROUND((resident_size / 1024 / 1024) ,0) AS "Memory WorkingSet MB",
+ROUND((total_size / 1024 / 1024) ,0) AS "Memory Commit MB",
+threads AS "Thread Count",
+ROUND((disk_bytes_read / 1024 / 1024 / 1024) ,0) AS "Disk Read GB",
+ROUND((disk_bytes_written / 1024 / 1024 / 1024) ,0) AS "Disk Write GB",
+ROUND((elapsed_time / 3600) ,0) AS "Uptime Hours",
+path
+
+--ROUND((total_size * '10e-7') ,0) AS Wert_2
+FROM processes
+
+where path like "%cisco%"
+OR name in ("acnvmagent.exe", "cscm.exe")
+
+
+
+ORDER BY total_size
+DESC LIMIT 100;
